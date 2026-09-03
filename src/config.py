515157f280309a3bb1f5fd9e781fc28b2427e377
Geofferin <syanczov@bk.ru>
2026-09-03 13:34:46 +0300

    починил файлы чтобы выполнить миграцию (после того как сам их и сломал)

--- src/config.py
+++ src/config.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+BASE_DIR = Path(__file__).resolve().parent.parent
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -5,7 +7,7 @@ class Settings(BaseSettings):
     postgres_url: str
 
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=BASE_DIR / ".env",
         env_file_encoding="utf-8",
         extra="ignore",
     )
